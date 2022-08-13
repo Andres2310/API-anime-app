ab64bd91f244fa7046f7b0348f0af9efdac42297
Andres2310 <and.23108@gmail.com>
2022-08-13 02:59:45 -0400

    add API create read mongo

---
+++ index.js
@@ -0,0 +1,86 @@
+require('dotenv').config();
+
+
+const express = require("express");
+const mongoose=require("mongoose");
+
+const mongoDBenv=process.env.DATABASE_URL;
+
+const Model = require('./model/model');
+
+mongoose.connect(mongoDBenv);
+const database=mongoose.connection;
+
+const bodyParser=require("body-parser")
+
+const app = express();
+
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(express.json());
+
+database.on("error",(error)=>{
+  console.log(error);
+});
+
+database.once('connected',()=>{
+  console.log("Conectado a mongoDB")
+});
+
+
+  let respuesta={
+    error: false,
+    codigo: 200,
+    mensaje:''
+  };
+
+  app.get('/', function(req, res) {
+    respuesta = {
+     error: false,
+     codigo: 200,
+     mensaje: 'Servidor Activo'
+    };
+    res.send(respuesta);
+   });
+
+
+
+   app.get('/productos', async (req, res) => {
+    try{
+        const data = await Model.find();
+        res.json(data)
+    }
+    catch(error){
+        res.status(500).json({message: error.message})
+    }
+})
+
+
+   app.post('/postProducto', async (req, res) => {
+    const data = new Model({
+        id: req.body.id,
+        name: req.body.name,
+        sizes: req.body.sizes,
+        category: req.body.category,
+        price: req.body.price,
+        quantity: req.body.quantity,
+        imageSrc: req.body.imageSrc
+    })
+
+    try {
+        const dataToSave = await data.save();
+        res.status(200).json(dataToSave)
+    }
+    catch (error) {
+        res.status(400).json({message: error.message})
+    }
+});
+
+
+
+
+
+app.listen(3000, () => {
+ console.log("El servidor está inicializado en el puerto 3000");
+});
